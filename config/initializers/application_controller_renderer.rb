cdcb4f1cb82190345b199680d95cd07447ded2d0
Naoya0827 <n.freedom.yazuna@gmail.com>
2021-11-04 12:18:39 +0000

    [Add]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a677b2f9331bb188eeb4a38377b20e33f9a4fdb31a8155af473055f2f47098159b526735f10f826dfcbcfd75f926184c1d9f2c54730f1ad245821a7ac1227d7e'
